20c4f4dbdf94949db27e07931aa4339cbbce5c33
Marcelo Amancio de Lima Santos <maxherogamer.contato@gmail.com>
2019-12-16 12:35:58 -0300

    Update quest.ex

--- lib/macchinista/cartello/quest.ex
+++ lib/macchinista/cartello/quest.ex
@@ -15,9 +15,11 @@ defmodule Macchinista.Cartello.Quest do
   @type name :: String.t
   @type checked :: boolean()
   @type checklist :: Checklist.t
+  @type order :: integer()
   @type t :: %__MODULE__{
     name: name,
     checked: checked,
+    order: order,
     checklist: checklist,
     inserted_at: NaiveDateTime.t,
     updated_at: NaiveDateTime.t
@@ -39,6 +41,7 @@ defmodule Macchinista.Cartello.Quest do
 
   schema "quests" do
     field :name, :string
+    field :order, :integer
     field :checked, :boolean
     belongs_to :checklist, Checklist
 
@@ -59,6 +62,18 @@ defmodule Macchinista.Cartello.Quest do
    quest
    |> change()
    |> put_change(:name, name)
+  end
+  
+  @spec get_order(t) :: order
+  def get_order(%__MODULE__{ order: order }), do: order
+
+  @spec set_order(type_or_changeset, order) :: changeset
+  def set_order(%Changeset{ data: %__MODULE__{} } = changeset, order),
+    do: put_change(changeset, :order, order)
+  def set_order(%__MODULE__{} = order, order) do
+   quest
+   |> change()
+   |> put_change(:order, order)
   end
 
   @spec get_checked(t) :: checked
